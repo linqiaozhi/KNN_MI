c4b831fb974742710ae10d80cabebbf33cc9eedf
George Linderman <george.linderman@yale.edu>
2017-05-18 09:20:06 -0400

    Changing k automatically

--- MI_KNN_cont_cat.m
+++ MI_KNN_cont_cat.m
@@ -1,4 +1,4 @@
-function [ MI ] = MI_KNN_cont_cat( X,Y,k )
+function [ MI ] = MI_KNN_cont_cat( X,Y,k_in )
 % MI_KNN_cat_cat - Estimates MI between continuous variable X and
 % categorical variable Y.  Y should be a vector of integers starting from
 % 1.
@@ -8,6 +8,12 @@ Y = Y - min(Y) +1;
 
 Y_tbl = tabulate(Y);
 Y_freqs = Y_tbl(:,3)*0.01;
+
+k = min(k_in, min(Y_tbl(:,2))/2 );
+if (k_in ~= k) 
+    fprintf('Warning: Changing K to be %d\n',  k);
+end
+
 MI = 0;
 N = length(X);
 nlevel = max(Y);
@@ -17,11 +23,7 @@ for i=1:nlevel,
     f_x_i = k./(2*N.*D(:,k));
     
     N_cond = sum(Y==i);
-    if (N_cond/2 < k),
-        fprintf('Error: The number of elements with Y= %d is %d, which is less than 2*k.  Skipping this one...', i, N_cond);
-        MI = 0;
-        return;
-    end
+
     [~, D_cond] = knnsearch(X(Y==i),X(Y==i), 'K',k);
     f_y_i_cond = k./(2*N_cond.*D_cond(:,k));
     
